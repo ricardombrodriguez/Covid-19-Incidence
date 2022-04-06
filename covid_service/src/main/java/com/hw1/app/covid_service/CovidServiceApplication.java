861058319041283358471178c33fbfa2a519f0ae
ricardombrodriguez <ricardorodriguez@ua.pt>
2022-04-06 23:43:10 +0100

    Project initialization - Angular + Spring Boot

---
+++ covid_service/src/main/java/com/hw1/app/covid_service/CovidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hw1.app.covid_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidServiceApplication.class, args);
+	}
+
+}
