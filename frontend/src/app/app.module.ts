f2ebf53d66cb8330d4218871a47b1942f631212e
ricardombrodriguez <ricardorodriguez@ua.pt>
2022-04-07 19:52:35 +0100

    Editting template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { StatisticComponent } from './components/statistic/statistic.component';
+import { GraphComponent } from './components/graph/graph.component';
+import { MapComponent } from './components/map/map.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { NewsComponent } from './components/news/news.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    StatisticComponent,
+    GraphComponent,
+    MapComponent,
+    CalendarComponent,
+    NewsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
